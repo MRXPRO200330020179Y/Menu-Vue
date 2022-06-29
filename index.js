28b9eb530e11723d083b014718b09056394e861c
marat <mrxpro2003@gmail.com>
2022-06-29 11:14:37 +0500

    Menu-Vue

---
+++ index.js
@@ -0,0 +1,13 @@
+new Vue({
+    el:'#app',
+    data:{
+        items:[
+            'Home',
+            'About',
+            'Contacti',
+            'Product',
+            'Blog',
+        ],
+        show:false
+    }
+})
